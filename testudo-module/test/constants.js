e8f374bf9185c0c7da2cba98a8811376cefb6358
Aaron Rivas <aarivas793@gmail.com>
2024-02-26 10:06:08 -0800

    enabled desktop config for lighthouse

---
+++ testudo-module/test/constants.js
@@ -0,0 +1,21 @@
+const desktopConfig = {
+  throttling: {
+    rttMs: 40,
+    throughputKbps: 10 * 1024,
+    cpuSlowdownMultiplier: 1,
+    requestLatencyMs: 0, // 0 means unset
+    downloadThroughputKbps: 0,
+    uploadThroughputKbps: 0,
+  },
+  screenEmulation: {
+    mobile: false,
+    width: 1350,
+    height: 940,
+    deviceScaleFactor: 1,
+    disabled: false,
+  },
+  emulatedUserAgent:
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+};
+
+export default desktopConfig;
