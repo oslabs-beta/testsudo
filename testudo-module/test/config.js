f160ef45089e1eb229b64a739e1c476134fb3d97
Aaron Rivas <aarivas793@gmail.com>
2024-02-27 19:11:47 -0800

    added configurations to be exactly like the configuration in devtools; variance should now be machine-specific

--- testudo-module/test/config.js
+++ testudo-module/test/config.js
@@ -7,26 +7,46 @@ const config = {
   desktopConfig: {
     extends: 'lighthouse:default',
     settings: {
-      maxWaitForFcp: 15 * 1000,
-      maxWaitForLoad: 35 * 1000,
+      // copied exactly from the lighthouse test parameters
+      additionalTraceCategories: null,
+      blankPage: 'about:blank',
+      blockedUrlPatterns: null,
+      budgets: null,
+      auditMode: false,
+      extraHeaders: null,
+      onlyAudits: null,
+      precomputedLanternData: null,
+      cpuQuietThresholdMs: 1000,
+      maxWaitForFcp: 30000,
+      debugNavigation: false,
+      disableFullPageScreenshot: false,
+      disableStorageReset: true,
+      maxWaitForLoad: 45000,
+      gatherMode: false,
+      pauseAfterFcpMs: 1000,
+      pauseAfterLoadMs: 1000,
       formFactor: 'desktop',
+      emulatedUserAgent:
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+      networkQuietThresholdMs: 1000,
       throttling: {
-        rttMs: 40,
-        throughputKbps: 10 * 1024,
         cpuSlowdownMultiplier: 1,
-        requestLatencyMs: 0, // 0 means unset
         downloadThroughputKbps: 0,
+        requestLatencyMs: 0,
+        rttMs: 40,
+        throughputKbps: 10240,
         uploadThroughputKbps: 0,
       },
       screenEmulation: {
-        mobile: false,
-        width: 1350,
-        height: 940,
-        deviceScaleFactor: 1,
-        disabled: false,
+        deviceScaleFactor: 1.75,
+        disabled: true,
+        height: 823,
+        mobile: true,
+        width: 412,
       },
-      emulatedUserAgent:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+      throttlingMethod: 'simulate',
+      usePassiveGathering: false,
+      skipAboutBlank: false,
       skipAudits: [
         // Skip the h2 audit so it doesn't lie to us. See https://github.com/GoogleChrome/lighthouse/issues/6539
         'uses-http2',
