7d87e2b85a84f0d8697af74e2956eb99ad5cf9ce
PavelKrapivin <pavelkrapivin.ny@gmail.com>
2024-03-13 20:28:01 -0400

    sync method

--- bearerScriptJson.js
+++ bearerScriptJson.js
@@ -2,50 +2,42 @@ import { exec } from 'child_process';
 import path from 'path';
 import { promisify } from 'util';
 
-const execAsync = promisify(exec);
+try {
+  // Find the absolute path of the current working directory
+  // const absolutePath = process.cwd();
+  const absolutePath =
+    '/Users/pavelkrapivin/Desktop/codesmith/testudo/web-app/server/controllers';
+  // Run the bearer scan command and output to a JSON file
+  const command = `bearer scan ${absolutePath} --format=json --output=scan_report.json`;
+  exec(command, { stdio: 'inherit' });
+} catch (error) {
+  console.error(error.message);
+}
 
 // const runBearerScript = async () => {
-//   // console.log(process.cwd());
-
 //   try {
 //     // Find the absolute path of the current working directory
 //     // const absolutePath = process.cwd();
 //     const absolutePath =
 //       '/Users/pavelkrapivin/Desktop/codesmith/testudo/web-app/server/controllers';
-//     // Run the bearer scan command and output to a JSON file
-//     const command = `bearer scan ${absolutePath} --format=json --output=scan_report.json`;
-//     await execAsync(command, { stdio: 'inherit' });
+//     // Run the bearer scan command and capture the output
+//     const command = `bearer scan ${absolutePath} --format=json`;
+//     const { stdout, stderr } = await execAsync(command);
+//     console.log('Log from the bearer script->>>', stdout);
+//     // Check for errors in stderr
+//     if (stderr) {
+//       console.error(stderr);
+//       throw new Error('Error running bearer scan command');
+//     }
+
+//     // Parse the JSON output
+//     const scanResult = JSON.parse(stdout);
+
+//     return scanResult;
 //   } catch (error) {
 //     console.error(error.message);
+//     throw error;
 //   }
 // };
 
 // export default runBearerScript;
-
-const runBearerScript = async () => {
-  try {
-    // Find the absolute path of the current working directory
-    // const absolutePath = process.cwd();
-    const absolutePath =
-      '/Users/pavelkrapivin/Desktop/codesmith/testudo/web-app/server/controllers';
-    // Run the bearer scan command and capture the output
-    const command = `bearer scan ${absolutePath} --format=json`;
-    const { stdout, stderr } = await execAsync(command);
-    console.log('Log from the bearer script->>>', stdout);
-    // Check for errors in stderr
-    if (stderr) {
-      console.error(stderr);
-      throw new Error('Error running bearer scan command');
-    }
-
-    // Parse the JSON output
-    const scanResult = JSON.parse(stdout);
-
-    return scanResult;
-  } catch (error) {
-    console.error(error.message);
-    throw error;
-  }
-};
-
-export default runBearerScript;
--- web-app/server/controllers/securityController.js
+++ web-app/server/controllers/securityController.js
@@ -1,44 +1,67 @@
-import runBearerScript from '../../../bearerScriptJson.js';
+// import runBearerScript from '../../../bearerScriptJson.js';
 import fs from 'fs';
 import path from 'path';
 
-const securityController = {};
+// const securityController = {};
 
-securityController.runBearerScript = async (req, res, next) => {
+// securityController.runBearerScript = async (req, res, next) => {
+//   try {
+//     console.log('running bearer script');
+//     await runBearerScript();
+//     res
+//       .status(200)
+//       .json({ message: 'Script completed. Sending success response.' });
+//   } catch (error) {
+//     // res.status(200).json({ message: 'Scan has been completed' });
+//     console.log(error);
+//     res.status(500).json({ error: 'Internal server error' });
+//     return next();
+//   }
+// };
+
+// securityController.readReport = (req, res, next) => {
+//   try {
+//     const reportFilePath = path.resolve(__dirname, '../../scan_report.json');
+//     const reportContent = fs.readFileSync(reportFilePath, 'utf-8');
+//     const reportData = JSON.parse(reportContent);
+
+//     console.log(reportData);
+//     res.json({ data: reportData });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: 'Internal server error' });
+//     return next();
+//   }
+// };
+
+import { execSync } from 'child_process';
+
+const runNpmScriptSync = (scriptName) => {
   try {
-    console.log('running bearer script');
-    await runBearerScript();
-    res
-      .status(200)
-      .json({ message: 'Script completed. Sending success response.' });
+    const command = `npm run ${scriptName}`;
+    const result = execSync(command, { encoding: 'utf-8' });
+
+    console.log(`npm script output: ${result}`);
+    console.log('npm script executed successfully');
   } catch (error) {
-    // res.status(200).json({ message: 'Scan has been completed' });
-    console.log(error);
-    res.status(500).json({ error: 'Internal server error' });
-    return next();
+    console.error(`Error executing npm script: ${error.message}`);
+    throw new Error(`Error executing npm script: ${error.message}`);
   }
 };
 
-securityController.readReport = (req, res, next) => {
-  try {
-    const reportFilePath = path.resolve(__dirname, '../../scan_report.json');
-    const reportContent = fs.readFileSync(reportFilePath, 'utf-8');
-    const reportData = JSON.parse(reportContent);
+const securityController = {};
 
-    console.log(reportData);
-    res.json({ data: reportData });
+securityController.runBearerScript = (req, res, next) => {
+  try {
+    console.log('running bearer script');
+    runNpmScriptSync('sec_json');
+    console.log('Script completed. Sending success response.');
+    res.status(200).json({ message: 'Scan has been completed' });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).json({ error: 'Internal server error' });
     return next();
   }
 };
 
 export default securityController;
-
-// const reportFilePath = path.resolve(__dirname, '../../scan_report.json');
-// const reportContent = fs.readFileSync(reportFilePath, 'utf-8');
-// const reportData = JSON.parse(reportContent);
-
-// res.json({ data: reportData });
-// console.log(reportData);
